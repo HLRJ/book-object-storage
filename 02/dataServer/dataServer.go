25904834fbc951575009008677e1dfa18e84d3c0
Zhenxiang Wang <befun@vip.qq.com>
2022-07-30 17:18:42 +0800

    fix: 导包错误

--- 02/dataServer/dataServer.go
+++ 02/dataServer/dataServer.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"book-object-storage/02/object"
+	"book-object-storage/02/dataServer/heartbeat"
+	"book-object-storage/02/dataServer/locate"
+	"book-object-storage/02/dataServer/objects"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	go heartbeat.StarHearbeat()
+	go heartbeat.StartHeartbeat()
 	go locate.StartLocate()
-	http.HandleFunc("/objects/", object.Handler)
+	http.HandleFunc("/objects/", objects.Handler)
 	// 由于是本地运行，将ip端口改成本地ip
 	log.Fatal(http.ListenAndServe("127.0.0.1"+os.Getenv("LISTEN_ADDRESS"), nil))
 }
