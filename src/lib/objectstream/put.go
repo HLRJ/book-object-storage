2c361944cae756fa083c8b531bb473f434d68094
Zhenxiang Wang <befun@vip.qq.com>
2022-07-30 09:16:22 +0800

    feat: add src/lib/objecetstream包

---
+++ src/lib/objectstream/put.go
@@ -0,0 +1,36 @@
+package objectstream
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type PutStream struct {
+	writer *io.PipeWriter //用于实现write方法
+	c      chan error     //用于把在一个goroutine传输数据的过程中发生的错误传回主线程。
+}
+
+func NewPutStream(server, object string) *PutStream {
+	reader, writer := io.Pipe() //创建一对reader和writer管道
+	c := make(chan error)
+	go func() {
+		request, _ := http.NewRequest("PUT", "http://"+server+"/objects/"+object, reader)
+		client := http.Client{}
+		r, e := client.Do(request)
+		if e == nil && r.StatusCode != http.StatusOK {
+			e = fmt.Errorf("dataServer return http code %d", r.StatusCode)
+		}
+		c <- e
+	}()
+	return &PutStream{writer, c}
+}
+
+func (w *PutStream) Write(p []byte) (n int, err error) {
+	return w.writer.Write(p)
+}
+
+func (w *PutStream) Close() error {
+	w.writer.Close()
+	return <-w.c
+}
