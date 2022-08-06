8d0ab93a4901bb69e005798904c58c6b7b0e6b3f
Zhenxiang Wang <befun@vip.qq.com>
2022-08-06 20:41:18 +0800

    chore: init 03/dataServer/dataServer.go

---
+++ 03/dataServer/dataServer.go
@@ -0,0 +1,17 @@
+package dataServer
+
+import (
+	"book-object-storage/03/dataServer/heartbeat"
+	"book-object-storage/03/dataServer/locate"
+	"book-object-storage/03/dataServer/objects"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	go heartbeat.StartHeartbeat()
+	go locate.StartLocate()
+	http.HandleFunc("/objects/", objects.Handler)
+	log.Fatal(http.ListenAndServe(os.Getenv("LISTEN_ADDRESS"), nil))
+}
