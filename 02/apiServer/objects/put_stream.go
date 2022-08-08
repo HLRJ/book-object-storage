416771e73045c2702c1949bb8aec3cd85567a4e5
Zhenxiang Wang <befun@vip.qq.com>
2022-08-08 18:12:41 +0800

    chore: add comment

--- 02/apiServer/objects/put_stream.go
+++ 02/apiServer/objects/put_stream.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 )
 
+// 用对象的名字，找到一个随机地可用的节点地址，然后将其
 func putStream(object string) (*objectstream.PutStream, error) {
+	// 找到一个能连接的节点，随机就可以
 	server := heartbeat.ChooseRandomDataServer()
 	if server == "" {
 		return nil, fmt.Errorf("cannot find any dataServer")
