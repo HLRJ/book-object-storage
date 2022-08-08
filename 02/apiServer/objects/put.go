cc4836e464788a14de2912860a9951149c13b737
Zhenxiang Wang <befun@vip.qq.com>
2022-08-08 18:12:57 +0800

    chore: add comment

--- 02/apiServer/objects/put.go
+++ 02/apiServer/objects/put.go
@@ -8,6 +8,7 @@ import (
 
 func put(w http.ResponseWriter, r *http.Request) {
 	object := strings.Split(r.URL.EscapedPath(), "/")[2]
+	// 不需要知道存储的服务器节点地址，找一个能存的就行
 	c, e := storeObject(r.Body, object)
 	if e != nil {
 		log.Println(e)
