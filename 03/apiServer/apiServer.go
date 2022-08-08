98c0eeacecb6d0a4eccea9407cb8f721e163e500
Zhenxiang Wang <befun@vip.qq.com>
2022-08-09 00:13:27 +0800

    chore: init 03/apiServer/

--- 03/apiServer/apiServer.go
+++ 03/apiServer/apiServer.go
@@ -1,15 +1,18 @@
 package main
 
 import (
-	"book-object-storage/02/apiServer/locate"
+	"book-object-storage/03/apiServer/heartbeat"
+	"book-object-storage/03/apiServer/locate"
+	"book-object-storage/03/apiServer/objects"
 	"book-object-storage/03/apiServer/version"
+
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	go heartbeat.ListenHeartBeat()
+	go heartbeat.ListenHeartbeat()
 	http.HandleFunc("/objects/", objects.Handler)
 	http.HandleFunc("/locate/", locate.Handler)
 	http.HandleFunc("/versions/", version.Handler)
